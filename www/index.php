1676f43cc72707f16960e5c1c02143179c5d80b7
Ruben2776 <ruben@live.dk>
2016-12-09 10:41:40 +0100

    Ruben, first upload

--- www/index.php
+++ www/index.php
@@ -1,2 +1,29 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="da">
+<head>
+
+	<!-- Site meta -->
+	<meta charset="utf-8">
+    <title>proplan</title>
+    
+    <!-- CSS -->
+    <link href='fullcalendar/fullcalendar.min.css' rel='stylesheet' />
+    <link href='fullcalendar/fullcalendar.print.min.css' rel='stylesheet' media='print' />
+
+    <script src='fullcalendar/lib/moment.min.js'></script>
+    <script src='fullcalendar/lib/jquery.min.js'></script>
+    <script src='fullcalendar/fullcalendar.min.js'></script>
+    
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>Min Uddanelse</li>
+            <li>Undervisning</li>
+            <li>Fravær</li>
+        </ul>
+    </nav>
+	<div id='calendar'></div>
+
+</body>
+</html>
